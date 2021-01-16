3cb065697c11688c5ef48ff44c431327f7f9ca26
Nina <nxtang789@gmail.com>
2021-01-16 14:58:44 -0500

    Redux Thunk

--- react/confusion/src/components/DishDetailComponent.js
+++ react/confusion/src/components/DishDetailComponent.js
@@ -3,7 +3,7 @@ import { Card, CardImg, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbIte
     Button, Modal, ModalHeader, ModalBody, Label, Row, Col} from 'reactstrap'
 import { Link } from "react-router-dom";
 import { Control, LocalForm, Errors } from 'react-redux-form';
-
+import { Loading } from "./LoadingComponent";
     
     function RenderDish({dish}) {
         // console.log(dish)
@@ -56,7 +56,24 @@ import { Control, LocalForm, Errors } from 'react-redux-form';
     }
 
     const DishDetail = (props) =>{
-
+        if(props.isLoading) {
+            return(
+                <div className="container">
+                    <div className="row">
+                        <Loading />
+                    </div>
+                </div>
+            );
+        }
+        else if (props.errMess) {
+            return(
+                <div className="container">
+                    <div className="row">
+                        <h4>{props.errMess}</h4>
+                    </div>
+                </div>
+            );
+        }
         // console.log(this.props.dishSelect)
         if(props.dish != null){
             return (
